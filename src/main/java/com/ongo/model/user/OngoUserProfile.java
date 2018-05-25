0f6d47a2d01d0e284809d4d8b043a05c2324329e
Muhammad Hussaini <mhussaini2012@gmail.com>
2018-05-25 20:29:13 +0100

    Added OngoUserProfile representing User Profile Data in the Database

---
+++ src/main/java/com/ongo/model/user/OngoUserProfile.java
@@ -0,0 +1,80 @@
+package com.ongo.model.user;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class OngoUserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstname;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(nullable = false)
+    private String email;
+    @Lob
+    private byte[] avatar;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public byte[] getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(byte[] avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OngoUserProfile that = (OngoUserProfile) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "OngoUserProfile{" +
+                "id=" + id +
+                '}';
+    }
+}
