b3eca7834744ef86c42ca35b6ff886f0b08ea520
Muhammad Hussaini <mhussaini2012@gmail.com>
2018-05-25 17:26:55 +0100

    Initial commit for the Ongo Project

---
+++ src/main/java/com/ongo/OngoApplication.java
@@ -0,0 +1,12 @@
+package com.ongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OngoApplication.class, args);
+    }
+}
