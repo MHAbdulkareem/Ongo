ae2c78235cbaf89b6744db0f267f0a11823c032c
Muhammad Hussaini <mhussaini2012@gmail.com>
2018-05-26 02:13:20 +0100

    Disabled Security @SpringBoot & ignore properties files

--- src/main/java/com/ongo/OngoApplication.java
+++ src/main/java/com/ongo/OngoApplication.java
@@ -2,8 +2,9 @@ package com.ongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class OngoApplication {
 
     public static void main(String[] args) {
