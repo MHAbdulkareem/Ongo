c89bad1ae2800f89301025c7159a71fe54d37a37
Muhammad Hussaini <mhussaini2012@gmail.com>
2018-05-30 03:07:10 +0100

    Added Spring WebMVC Configuration

---
+++ src/main/java/com/ongo/MvcWebConfig.java
@@ -0,0 +1,32 @@
+package com.ongo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.GzipResourceResolver;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+@EnableWebMvc
+public class MvcWebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**", "/public/**")
+                .addResourceLocations("classpath:/public/")
+                .setCachePeriod(3600)
+                .resourceChain(false)
+                .addResolver(new GzipResourceResolver())
+                .addResolver(new PathResourceResolver());
+    }
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
