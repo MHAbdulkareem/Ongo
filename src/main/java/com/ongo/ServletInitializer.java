fa8d986ee8ced06e05f5a04025bc58dcc7cdcc50
Muhammad Hussaini <mhussaini2012@gmail.com>
2018-06-25 20:24:12 +0100

    Enabled Servlet Container and Tomcat

---
+++ src/main/java/com/ongo/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.ongo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OngoApplication.class);
+    }
+}
